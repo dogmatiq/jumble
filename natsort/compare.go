f01427c887efa46f100bd440216dca6c9b4daf9a
James Harris <contact@jamesharris.id.au>
2024-03-03 13:27:38 +1000

    Improve documentation.

--- natsort/compare.go
+++ natsort/compare.go
@@ -1,8 +1,9 @@
 package natsort
 
-// Compare returns an integer comparing two strings "naturally", such that
-// numeric elements of the strings are sorted numerically, instead of
-// lexigraphically.
+// Compare performs a three-way comparison of x and y. Strings of digits within
+// the strings are compared using their integer value.
+//
+// The result will be 0 if x == y, -1 if x < y, and +1 if x > y.
 func Compare[T ~string](x, y T) int {
 	if x == y {
 		return 0
@@ -21,8 +22,8 @@ func Compare[T ~string](x, y T) int {
 	}
 }
 
-// Less returns true if x sorts before y, otherwise false, such that numeric
-// elements of the strings are sorted numerically, instead of lexigraphically.
+// Less returns true if x should appear before y in a sorted list. Strings of
+// digits within the strings are compared using their integer value.
 func Less[T ~string](x, y T) bool {
 	return Compare(x, y) < 0
 }
